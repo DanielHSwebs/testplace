5334744a61afa5017a2cdbcd8bae66128adb0815
DanielHSwebs <40626839+DanielHSwebs@users.noreply.github.com>
2018-08-06 18:02:16 +0200

    first thinngs

---
+++ js/game.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+ctx.moveTo(0,0);
+ctx.lineTo(200,100)
+ctx.stroke();
+
+/* ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
+*/
+
